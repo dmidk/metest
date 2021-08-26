7459e11008cee76a90067e0618154a2dd0144b4f
khintz <kah@dmi.dk>
2021-08-26 12:12:53 +0200

    Set up interface

--- metest/__main__.py
+++ metest/__main__.py
@@ -13,18 +13,14 @@ __email__ = "kah@dmi.dk"
 __status__ = "Development"
 
 import sys
+import os
+sys.path.insert(0, os.path.abspath('./metest/'))
 import argparse
 from argparse import ArgumentDefaultsHelpFormatter
 
-def test(test:None) -> None:
-    """summary
+from .logmetric import logmetric
+from .datametric import datametric
 
-    Parameters
-    ----------
-    test : None
-        description
-    """
-    return
 
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
@@ -33,19 +29,53 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 
-
-
-
 if __name__ == '__main__':
-    parser = MyParser(description='Test, check and compare logs and data from meteorological computations.',
-                  formatter_class=ArgumentDefaultsHelpFormatter)
+    parent_parser = MyParser(
+        description='Test, check and compare logs and data from meteorological computations.',
+        formatter_class=ArgumentDefaultsHelpFormatter)
+    
+    subparser = parent_parser.add_subparsers(dest="cmd")
 
-    parser.add_argument('-v',
-                        '--verbose',
-                        action='store_true',
-                        help='Print actions to console')
+    parser_logmetric = subparser.add_parser('logmetric', help='Get metrics from logfile')
+    parser_datametric = subparser.add_parser('datametric', help='Get metrics from output data')
 
-    args = parser.parse_args()
+    parser_logmetric.add_argument('-m',
+                    '--model',
+                    metavar='MODEL',
+                    type=str,
+                    help='Which model are logfiles coming from (harmonie)',
+                    required=True)
+    
+    parser_logmetric.add_argument('-c',
+                    '--compare',
+                    action='store_true',
+                    help='Compute metrics from comparing two logfiles')
 
-    if args.verbose:
-        print('Verbose mode: ON', flush=True)
+    parser_logmetric.add_argument('-i',
+                    '--individual',
+                    action='store_true',
+                    help='Compute metrics from one logfile')
+
+    parser_logmetric.add_argument('-f',
+                    '--file',
+                    metavar='FILE',
+                    type=str,
+                    help='Path to logfile to read',
+                    required=True)
+
+    parser_logmetric.add_argument('-f2',
+                    '--file2',
+                    metavar='FILE2',
+                    type=str,
+                    help='Path to second logfile to read',
+                    required=False)
+
+
+    if len(sys.argv)==1:
+        parent_parser.print_help()
+        sys.exit(2)
+
+    args = parent_parser.parse_args()
+
+    if args.cmd == 'logmetric':
+        logwork = logmetric(args)
